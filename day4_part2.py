498c04708d2700b00d1df5d55c84397d413407c2
Sneha Rajbhar <121350364+Sneharajbhar@users.noreply.github.com>
2025-07-22 17:57:13 +0530

    Delete day4_part2.py

--- day4_part2.py
+++
@@ -1,7 +0,0 @@
-def sumOfNaturalNumber(num):
-    sum = 0
-    if num%2!=0: 
-        for i in range(1,num+1):
-           sum+=i # sum=sum+i compound operator 
-        print(sum)
-sumOfNaturalNumber(50)
